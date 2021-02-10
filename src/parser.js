e94125fa0dfe04e83a57d8077e28bca527c9c5f8
Sam Crowley <samxcrowley@gmail.com>
2021-02-11 09:29:07 +1100

    Initial commit

---
+++ src/parser.js
@@ -0,0 +1,152 @@
+/*
+
+Infix notation e.g.: "A ^ 2 + 2 * A * B + B ^ 2" (operators in between operands)
+RPN notation e.g.: "A 2 ^ 2 A * B * + B 2 ^ +" (operators after operands)
+
+infixToRPN algorithm adapted from https://www.andreinc.net/2010/10/05/converting-infix-to-rpn-shunting-yard-algorithm
+calculateRPN algorithm adapted from https://codereview.stackexchange.com/questions/120451/reverse-polish-notation-calculator-in-java
+
+*/
+
+class Parser {
+    
+    constructor() {
+        this.operators = ['+', '-', '*', '/', '^'];
+    }
+    
+    // converts a string in Infix notation to RPN notation
+    infixToRPN(infixString) {
+        
+        var output = "";
+        var stack = [];
+        
+        var tokens = this.tokenize(infixString);
+        
+        for (let i = 0; i < tokens.length; i++) {
+            
+            var token = tokens[i];
+            
+            if (this.operators.includes(token)) {
+                
+                while (stack.length != 0 && this.operators.includes(stack[0])) {
+                    
+                    var currOper = this.getOperationFromSymbol(token);
+                    var lastOper = this.getOperationFromSymbol(stack[0]);
+                    
+                    if ((currOper.leftAssociative && currOper.comparePrecedence(lastOper) <= 0) ||
+                       (!currOper.leftAssociative && currOper.comparePrecedence(lastOper) < 0)) {
+                        
+                        output = output + stack.shift() + " ";
+                        continue;
+                    }
+                    
+                    break;
+                    
+                }
+                
+                stack.unshift(token);
+                
+            } else if (token === "(") {
+                
+                stack.unshift(token);
+                
+            } else if (token === ")") {
+                
+                while (stack.length != 0 && stack[0] !== "(") {
+                    output = output + stack.shift(token) + " ";
+                }
+                
+                stack.shift();
+                
+            } else {
+                output = output + token + " ";
+            }
+            
+        }
+        
+        while (stack.length != 0) {
+            output = output + stack.shift() + " ";
+        }
+        
+        output = output.substr(0, output.length - 1);
+        
+        return output;
+        
+    }
+    
+    // returns the result after calculating an RPN string
+    calculateRPN(rpnString) {
+        
+        var stack = [];
+        
+        var tokens = rpnString.split(" ");
+        
+        for (let i = 0; i < tokens.length; i++) {
+            
+            var token = tokens[i];
+            
+            if (this.operators.includes(token)) {
+                
+                var x = parseFloat(stack.shift());
+                var y = parseFloat(stack.shift());
+                
+                var result = 0;
+                
+                switch (token) {
+                    case '+':
+                        result = x + y;
+                        break;
+                    case '-':
+                        result = y - x;
+                        break;
+                    case '*':
+                        result = x * y;
+                        break;
+                    case '/':
+                        result = y / x;
+                        break;
+                    case '^':
+                        result = Math.pow(y, x);
+                        break;
+                }
+                
+                stack.unshift(result);
+                
+            } else {
+                stack.unshift(token);
+            }
+            
+        }
+        
+        return stack.shift();
+        
+    }
+    
+    getOperationFromSymbol(symbol) {
+        
+        switch (symbol) {
+            case '+':
+                return new AdditionOperation('0', '0');
+                break;
+            case '-':
+                return new SubtractionOperation('0', '0');
+                break;
+            case '*':
+                return new MultiplicationOperation('0', '0');
+                break;
+            case '/':
+                return new DivisionOperation('0', '0');
+                break;
+            case '^':
+                return new IndexOperation('0', '0');
+                break;
+        }
+        
+    }
+    
+    // splits a string into separate tokens; digits, parentheses, letters, etc.
+    tokenize(str) {
+        return str.replace(/\s+/g, "").match(/(?:(?<!\d)-)?\d+(?:\.\d+)?|./g);
+    }
+    
+}
---
+++ src/sketch.js
@@ -0,0 +1,185 @@
+let WIDTH = 900;
+let HEIGHT = 600;
+
+let FONT_SIZE = 14;
+
+var inputField;
+var inputButton;
+
+var parser;
+
+var screenX;
+var screenY;
+
+var dragging;
+
+var zoom = 1.00;
+var zMin = 0.05;
+var zMax = 9.00;
+var sensativity = 0.005;
+
+function setup() {
+    
+    createCanvas(WIDTH, HEIGHT, WEBGL);
+    
+    MATHSFONT_REGULAR = loadFont("font/Montserrat-Regular.otf");
+    MATHSFONT_LIGHT = loadFont("font/Montserrat-Light.otf");
+    MATHSFONT_BOLD = loadFont("font/Montserrat-Bold.otf");
+    
+    textSize(FONT_SIZE);
+    textFont(MATHSFONT_REGULAR);
+    
+    inputField = createInput();
+    inputField.position(20, HEIGHT + 10);
+    
+    inputButton = createButton('Plot');
+    inputButton.position(inputField.x + inputField.width + 20, HEIGHT + 10);
+    inputButton.mousePressed(plot);
+    
+    parser = new Parser();
+    
+    screenX = 0;
+    screenY = 0;
+    
+    lastDragX = 0;
+    lastDragY = 0;
+    
+}
+
+function plot() {
+    
+    
+    
+}
+
+function mouseDragged() {
+    
+    if (!dragging) {
+        lastDragX = mouseX;
+        lastDragY = mouseY;
+    }
+    
+    dragging = true;
+    
+    var xDiff = mouseX - lastDragX;
+    var yDiff = mouseY - lastDragY;
+    
+    screenX += xDiff;
+    screenY += yDiff;
+    
+    lastDragX = mouseX;
+    lastDragY = mouseY;
+    
+}
+
+function mouseReleased() {
+    dragging = false;
+}
+
+function mouseWheel(event) {
+    zoom -= sensativity * event.delta;
+    zoom = constrain(zoom, zMin, zMax);
+    return false;
+}
+
+function draw() {
+    
+    background(17);
+    
+    stroke(255);
+    strokeWeight(2);
+    fill(255);
+    
+    scale(zoom);
+    translate(screenX, screenY);
+    
+    var xMargin = 0;
+    var yMargin = 200;
+    
+    var xRange = 20;
+    var yRange = 40;
+    
+    var xSize = 25;
+    var ySize = 25;
+    
+    drawAxes(xMargin, yMargin, xRange, yRange, xSize, ySize);
+    
+    // draw parabola
+    stroke(255, 0, 0);
+    for (var x = -5; x < 5; x++) {
+        
+        var y = Math.pow(x, 2);
+        var yNext = Math.pow(x + 1, 2);
+        
+        var xOff = (x * xSize);
+        var yOff = -y * ySize;
+        
+        var nextXOff = (x + 1) * xSize;
+        var nextYOff = -yNext * ySize;
+        
+        line(xMargin + xOff, yMargin + yOff, xMargin + nextXOff, yMargin + nextYOff);
+        
+    }
+    
+}
+
+function drawAxes(xMargin, yMargin, xRange, yRange, xSize, ySize) {
+    
+    var lowX = -(xRange / 2);
+    var lowY = -(yRange / 2);
+    var highX = xRange / 2;
+    var highY = yRange / 2;
+    
+    // draw x-axis line
+    line(xMargin - ((xRange / 2) * xSize), yMargin, xMargin + ((xRange / 2) * xSize), yMargin)
+    
+    // draw x-axis number markings
+    for (var x = lowX; x <= highX; x++) {
+        
+        //if (x % 2 != 0) continue;
+        if (x == 0) continue;
+        
+        var xPos = xMargin + (x * xSize);
+        line(xPos, yMargin - 5, xPos, yMargin + 5);
+        
+        s = str(x);
+        sWidth = textWidth(s);
+        text(s, xPos - (sWidth / 2), yMargin + 25);
+        
+    }
+    
+    // draw y-axis
+    line(xMargin, yMargin - ((yRange / 2) * ySize), xMargin, yMargin + ((yRange / 2) * ySize))
+    
+    // draw y-axis number markings
+    for (var y = lowY; y <= highY; y++) {
+        
+        //if (y % 2 != 0) continue;
+        if (y == 0) continue;
+
+        var yPos = yMargin + (y * ySize);
+        line(xMargin - 5, yPos, xMargin + 5, yPos);
+        
+        s = str(y);
+        sWidth = textWidth(s);
+        text(s, xMargin + 15, yPos - (sWidth / 2));
+
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
